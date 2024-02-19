8887054c1db4a600ffe3e860419c36675f14c431
John Palmer <johnrbpalmer@gmail.com>
2024-02-20 00:59:18 +0100

    added download from zenodo option

--- R/get_malert_data.R
+++ R/get_malert_data.R
@@ -1,30 +1,34 @@
 #' Download Mosquito Alert report data from GitHub
 #'
-#' @param source String. Source to download from. Currently only github works.
+#' @param source String. Source to download from. Options are github or zenodo.
+#' @param doi String. Zenodo doi if downloading from Zenodo. Default is the doi that will always point to the most recent version: 10.5281/zenodo.597466.
 #' @returns A tibble.
 #' @export
 #' @examples
 #' malert_reports = get_malert_data(source = "github")
 #' malert_reports
-get_malert_data = function(source = "github", doi = NA){
-    temp <- tempfile()
+get_malert_data = function(source = "zenodo", doi = "10.5281/zenodo.597466"){
+    this_temp_file <- tempfile()
     if(source == "github"){
+      temp = this_temp_file
     download.file("https://github.com/MosquitoAlert/Data/raw/master/all_reports.zip", destfile = temp)
     } else if(source == "zenodo" & !is.na(doi)){
-      dir.create(temp, showWarnings = FALSE)
-      n2khab::download_zenodo(doi = doi, path = temp)
-      temp = file.path(temp, list.files(temp))
-      unz(temp, file = this_file)
+      dir.create(this_temp_file, showWarnings = FALSE)
+      n2khab::download_zenodo(doi = doi, path = this_temp_file)
+      this_file = list.files(this_temp_file)
+      this_temp_file_zip = file.path(this_temp_file, list.files(this_temp_file))
+      outer_file_name = unzip(this_temp_file_zip, exdir = temp, list = TRUE)[1,1]
+      unzip(this_temp_file_zip, exdir = this_temp_file)
+      temp = file.path(this_temp_file, outer_file_name, "all_reports.zip")
     } else{
-  stop("Error: This function currently only supports downloads from Github")
+  stop("Error: This function currently only supports downloads from Github or Zenodo")
 }
     reports = bind_rows(lapply(2014:lubridate::year(lubridate::today()), function(this_year){
       print(this_year)
       flush.console()
       this_file = paste0("home/webuser/webapps/tigaserver/static/all_reports", this_year, ".json")
-      jsonlite::fromJSON(unz(temp, file = this_file), flatten = TRUE) %>% as_tibble()
+      jsonlite::fromJSON(unz(temp_test, file = this_file), flatten = TRUE) %>% as_tibble()
     }))
-    unlink(temp)
+    unlink(this_temp_file)
     return(reports)
-
 }
